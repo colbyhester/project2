4e5568d9a6b8a1d98aa291d42f367e59b56d65a4
Sandra <sandradeann94@gmail.com>
2018-10-20 19:27:51 -0400

    find distance between zip codes

--- GetDistanceZip.js
+++ GetDistanceZip.js
@@ -1,27 +1,45 @@
 //FUNCTION TO INITIALIZE THE GOOGLE MAP
 function initialize() {
-    var zipCodesToLookup = new Array('20120', '20122', '22033', '22101', '20124', '20151');
-    var output = '<tr><th scope="col">From</th><th scope="col">To</th><th scope="col">Miles</th></tr>';
+    //zip codes: '22033', '22101', '20124', '20151'
+    //var zipCodesToLookup = new Array('20120', '20122');
 
-    //EXECUTE THE DISTANCE MATRIX QUERY
-    var service = new google.maps.DistanceMatrixService();
-    service.getDistanceMatrix({
-        origins:      zipCodesToLookup,
-        destinations: zipCodesToLookup,
-        travelMode:   google.maps.TravelMode.DRIVING,
-        unitSystem:   google.maps.UnitSystem.IMPERIAL
-    }, function(response, status) {
-        //...response processed here...//
-        if(status == google.maps.DistanceMatrixStatus.OK) {
-            var origins = response.originAddresses;
-            var destinations = response.destinationAddresses;
+    $("#submit").on('click', function () {
 
-            for(var i=0; i < origins.length; i++) {
-                var results = response.rows[i].elements;
-                for(var j=0; j < results.length; j++) {
-                output += '<tr><td>' + origins[i] + '</td><td>' + destinations[j] + '</td><td>' + results[j].distance.text + '</td></tr>'; }}
-            document.getElementById('zip_code_output').innerHTML = '<table cellpadding="5">' + output + '</table>';
+        //create new array
+        var zipCodesToLookup = new Array();
 
-        }
+        //get values from input box
+        zipCodesToLookup.push($("#to").val());
+        zipCodesToLookup.push($("#from").val());
+    
+        //EXECUTE THE DISTANCE MATRIX QUERY
+        var service = new google.maps.DistanceMatrixService();
+        service.getDistanceMatrix({
+            origins:      zipCodesToLookup,
+            destinations: zipCodesToLookup,
+            travelMode:   google.maps.TravelMode.DRIVING,
+            unitSystem:   google.maps.UnitSystem.IMPERIAL
+        }, function(response, status) {
+            //...response processed here...//
+            if(status == google.maps.DistanceMatrixStatus.OK) {
+                var origins = response.originAddresses;
+                var destinations = response.destinationAddresses;
+
+                for(var i=0; i <origins.length; i++) {
+                    var results = response.rows[i].elements;
+                    for(var j=0; j < results.length; j++) {
+                        //do not add row for same zip codes. ex: reston to reston (useless information)
+                        if(origins[i] != destinations[j]){
+                            $("#results").append("<tr><td>"+ results[j].distance.text + "</td></tr>");
+                            console.log(results[j].distance.text);
+                        } // end if
+    
+                    } //end inner for loop
+                } //end outer for loop
+            }//end status if
+        });
+
     });
+
+   
 }
