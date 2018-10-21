6974b3fa26abd4024106db7a64b287540bceb91d
Sandra <sandradeann94@gmail.com>
2018-10-20 21:45:44 -0400

    updated find distance

--- GetDistanceZip.js
+++ GetDistanceZip.js
@@ -1,8 +1,5 @@
 //FUNCTION TO INITIALIZE THE GOOGLE MAP
 function initialize() {
-    //zip codes: '22033', '22101', '20124', '20151'
-    //var zipCodesToLookup = new Array('20120', '20122');
-
     $("#submit").on('click', function () {
 
         //create new array
@@ -11,7 +8,7 @@ function initialize() {
         //get values from input box
         zipCodesToLookup.push($("#to").val());
         zipCodesToLookup.push($("#from").val());
-    
+
         //EXECUTE THE DISTANCE MATRIX QUERY
         var service = new google.maps.DistanceMatrixService();
         service.getDistanceMatrix({
@@ -22,24 +19,16 @@ function initialize() {
         }, function(response, status) {
             //...response processed here...//
             if(status == google.maps.DistanceMatrixStatus.OK) {
-                var origins = response.originAddresses;
-                var destinations = response.destinationAddresses;
+                //var origins = response.originAddresses;
+                //var destinations = response.destinationAddresses;
 
-                for(var i=0; i <origins.length; i++) {
-                    var results = response.rows[i].elements;
-                    for(var j=0; j < results.length; j++) {
-                        //do not add row for same zip codes. ex: reston to reston (useless information)
-                        if(origins[i] != destinations[j]){
-                            $("#results").append("<tr><td>"+ results[j].distance.text + "</td></tr>");
-                            console.log(results[j].distance.text);
-                        } // end if
-    
-                    } //end inner for loop
-                } //end outer for loop
-            }//end status if
-        });
+                var results = response.rows[0].elements;
+                //console.log(results);
+                $("#results").append("<tr><td>"+ results[1].distance.text + "</td></tr>");
+                    
+            }// end status if
 
-    });
+        });// end getDistanceMatrix
 
-   
+    });// end submit onclick
 }
